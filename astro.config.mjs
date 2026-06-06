35233f8f53b387786768e352e5dd5947dc6b07fb
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2026-06-06 12:35:05 +0200

    add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://teplo-hub.web.app',
+  integrations: [sitemap()],
+});
