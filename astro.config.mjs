15b4217b8b9fa2ac0f521c759e518a4529c8ce7b
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2026-06-25 19:38:12 +0200

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://termo-hub.web.app',
+  site: 'https://termo-hub.com.ua',
   integrations: [sitemap()],
 });
